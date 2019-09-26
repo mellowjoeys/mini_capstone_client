f046f25d2079f42734a6bad853da42d5d281448e
mellowjoeys <mellowjoey@gmail.com>
2019-09-26 11:34:47 -0500

    added edit action

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -9,10 +9,49 @@ class Client::ProductsController < ApplicationController
   def new
     render 'new.html.erb'
   end
-  
+
+  def create
+    response = HTTP.post(
+                          "http://localhost:3000/api/products",
+                          form: {
+                                  name: params[:name],
+                                  price: params[:price],
+                                  description: params[:description],
+                                  image_url: params[:image_url]
+                                }
+                        )
+    @product = response.parse
+    redirect_to "/client/products/#{@product['id']}"
+  end
+
   def show
     response = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
     @product = response.parse
     render 'show.html.erb'
+  end
+
+  def edit
+    response = HTTP.get("http://localhost:3000/api/products/#{params[:id]}")
+    @product = response.parse
+    render 'edit.html.erb'
+  end
+
+  def update
+    response = HTTP.patch(
+                          "http://localhost:3000/api/products/#{params[:id]}",
+                          form: {
+                                  name: params[:name],
+                                  price: params[:price],
+                                  description: params[:description],
+                                  image_url: params[:image_url]
+                                }
+                          )
+    @product = response.parse
+    redirect_to "/client/products/#{@product['id']}"
+  end
+
+  def destroy
+    response = HTTP.delete("http://localhost:3000/api/products/#{params[:id]}")
+    redirect_to "/client/products"
   end
 end
